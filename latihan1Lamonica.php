a4e04c46c8fece0f38a5c65fdbffdd3f4bc4f107
lamonicanandaputri <80017704+lamonicanandaputri@users.noreply.github.com>
2021-03-04 09:55:37 +0700

    Add files via upload

---
+++ latihan1Lamonica.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title> Belajar html dan php</title>
+</head>
+<body>Hallo <br>
+
+<p><b>Nama : Lamonica Nanda Putri</b><br></p>
+
+<p>Kelas : Komputerisasi Akuntansi Pagi 2018<br></p>
+
+UNIVERSITAS STEKOM AMBARAWA<br>
+</body>
+
+</html>
