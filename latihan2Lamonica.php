5dec0c12e67590f43c70c60d5066d67cad32d337
lamonicanandaputri <80017704+lamonicanandaputri@users.noreply.github.com>
2021-03-04 09:57:05 +0700

    Add files via upload

---
+++ latihan2Lamonica.php
@@ -0,0 +1,10 @@
+<?php
+	//kita akan membuat sebuah variabel dalam php
+	$namadepan = "Lamonica";
+	$namatengah = "Nanda";
+	$namabelakang = "Putri";
+	$jumlahbarang = 2;
+	$harga = 300000;
+	
+	echo "nama saya adalah $namadepan $namatengah $namabelakang saya membeli boneka sebanyak $jumlahbarang seharga $harga";
+	?>
